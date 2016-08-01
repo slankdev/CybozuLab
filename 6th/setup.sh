3c54e5cc124fa28e4c68e613603a92bfc1e5995a
slank@cybozu <slank.dev@gmail.com>
2016-08-01 17:36:58 +0900

    [CLEAN] some codes edited at 2016.08.01-17:36

--- 6th/setup.sh
+++ 6th/setup.sh
@@ -2,7 +2,7 @@
 #!/bin/sh
 
 echo '' > ./day_$(date +%m%d).md
-echo '[サイボウズ・ラボユース日時報告書]'
+echo '[サイボウズ・ラボユース日時報告書]' >> ./day_$(date +%m%d).md
 echo "勤務日：$(date '+%Y年%b%d日%a曜日')" >> ./day_$(date +%m%d).md
 cat  template/day.md >> ./day_$(date +%m%d).md
 cp template/report.txt .
